edb99bd77e974b44ba7c9c9117cac30f4af8b6f4
victorl <viclaxton@hotmail.com>
2016-03-11 15:26:20 -0500

    added java from browsy

--- app/src/main/java/com/victorl/numbershapes/MainActivity.java
+++ app/src/main/java/com/victorl/numbershapes/MainActivity.java
@@ -13,11 +13,55 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    class Number {
+        int number;
+
+        public boolean isSquare() {
+
+            double squareRoot = Math.sqrt(number);
+
+            if (squareRoot == Math.floor(squareRoot)) {
+                return true;
+
+            } else {
+                return false;
+            }
+        }
+
+        public boolean isTriangular() {
+
+            int x = 1;
+
+            int triangularNumber = 1;
+
+            while (triangularNumber < number) {
+
+                x++;
+
+                triangularNumber = triangularNumber + x;
+            }
+
+            if (triangularNumber == number)  {
+
+                return true;
+            } else {
+
+                return false;
+            }
+        }
+    }
+
+
+
+
     public void testNumber(View view){
 
         EditText usersNumber = (EditText) findViewById(R.id.usersNumber);
 
-        Log.i("usersNumber", usersNumber.getText().toString());
+        Number myNumber = new Number ();
+        myNumber.number = Integer.parseInt(usersNumber.getText().toString());
+
+        System.out.println(myNumber.isSquare());
 
     }
 
